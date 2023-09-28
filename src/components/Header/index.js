41b1c54617105b5203088a2834906a64e2e24596
Thien <toihctatca2022@gmail.com>
2023-09-28 08:29:52 +0700

    lesson9

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,7 @@ import ThemeContext from "../../contexts/ThemeContext";
 
 function Header() {
   const {theme, setTheme} = useContext(ThemeContext)
+  console.log(theme)
   return (
     <div className="header">
       <select value={theme} onChange={(e) => setTheme(e.target.value)}>
